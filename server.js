5e4aa44aef3bb59591a23b31827b8fcd500e35d6
vaibhav shinde <vaibhav65632@gmail.com>
2023-07-09 23:21:09 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors("*"));
+
+app.get("/", (req, res) => {
+  res.send("Hello from the Jenkins The Butler !!!");
+});
+
+app.listen(4000, "0.0.0.0", () => {
+  console.log("Server has started on port 4000 !!!");
+});
